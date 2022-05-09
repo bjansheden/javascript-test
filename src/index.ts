d669926a6f137841fc14d619378700b8748f7c42
Billy Jansheden <billyjansheden@gmail.com>
2022-05-09 14:12:35 +0200

    Experimenting with fetching data
    Utilizing async + await to fetch data.

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,32 @@ interface Video {
   grade: number;
 }
 
-console.log("Checking if it works")
-
-//GET http://localhost:3000/videos
-const getVideos = (): Promise<Video[]> => {
+const renderVideos = async () => {
+  let uri = 'http://localhost:3000/videos';
+  const res = await fetch(uri);
+  const videos = await res.json();
+  console.log(videos);
 }
 
-// PUT http://localhost:3000/videos/${id}
+window.addEventListener('DOMContentLoaded', () => renderVideos());
+
+//GET http://localhost:3000/videos
+function getVideos(): Promise<Video[]> {
+  return fetch('/db.json')
+  .then(res => res.json())
+  .then(res => {
+    return res as Video[]
+  })
+}
+
+const result = document.getElementById('result')
+//getVideos()
+        //.then(videos => {
+        //        result.innerHTML = videos.map(v => v.title).toString()
+       // })
+
+console.log(result)
+
+
+        // PUT http://localhost:3000/videos/${id}
 const updateVideo = async (id, changes): Promise<Video> => {};
