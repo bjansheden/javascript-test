df771e2fabdb1d15ca5d37fb94ca6a129795b87f
Billy Jansheden <billyjansheden@gmail.com>
2022-05-14 17:42:19 +0200

    New functionalities
    Added the ability to search for a movie in using a full-text. At the moment it searches everything, should limit this to title and grade.

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,62 @@ interface Video {
   grade: number;
 }
 
-const renderVideos = async () => {
-  let uri = 'http://localhost:3000/videos';
-  const res = await fetch(uri);
-  const videos = await res.json();
-  console.log(videos);
-}
 
-window.addEventListener('DOMContentLoaded', () => renderVideos());
+const container = document.querySelector('.videos') as HTMLDivElement;
+const searchForm = document.querySelector('.search') as HTMLFormElement;
+const updateGrade = document.querySelector('.videos') as HTMLButtonElement;
+
+const renderVideos = async (term: String) => {
+  if (term) {
+  let uri = 'http://localhost:3000/videos';
+  uri += `?q=${term}`;
+ 
+  const res = await fetch(uri);
+  const videos: Video[] = await res.json();
+  console.log(videos);
+
+  let template = ''
+  videos.forEach(video => {
+    template += `
+      <div class="video">
+        <h2>${video.title}</h2>
+        <p>Grade: ${video.grade}
+          <form>  
+            <label for="grade"> Edit grade </label>
+            <select name="grade" id="membership">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <input type="submit" value="Submit"/>
+          </form>
+        </p>
+      </div>
+      `
+    
+  })
+  container.innerHTML = template;
+  console.log(template);
+  }}
+
+updateGrade.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const result = await fetch('http://localhost:3000/videos/',{
+  method: 'PUT'
+  })
+  console.log(result)
+})
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  renderVideos(searchForm.term.value)
+  console.log(searchForm.term.value)
+})
+
+//window.addEventListener('DOMContentLoaded', () => renderVideos());
+
 
 //GET http://localhost:3000/videos
 function getVideos(): Promise<Video[]> {
